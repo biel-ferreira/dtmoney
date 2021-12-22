f857f9515ba9f93e53cdfd021a8fc4a654ce058f
biel-ferreira <93892548+biel-ferreira@users.noreply.github.com>
2021-12-22 00:48:23 -0300

    importando componente newtransacctionmodal

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,21 @@
+import { useState } from "react";
 import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
+import { NewTransactionModal } from "./components/NewTransactionModalOpen";
 import { GlobalStyle } from "./styles/global";
 
 export function App() {
+  const [isNewTransactionModalOpen, setIsNewTranscationModalOpen] = useState(false);
+    function handleOpenNewTransactionModal () {
+        setIsNewTranscationModalOpen(true)
+    }
+    function handleCloseNewTransactionModal () {
+        setIsNewTranscationModalOpen(false)
+    }
   return (
     <>
-      <Header/>
+      <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
+      <NewTransactionModal isOpen={isNewTransactionModalOpen} onRequestClose={handleCloseNewTransactionModal}/>
       <Dashboard/>
       <GlobalStyle/>
     </>
