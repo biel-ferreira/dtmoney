051aa616e624782d1554ad23a0a8b6e675f77359
biel-ferreira <93892548+biel-ferreira@users.noreply.github.com>
2021-12-22 11:54:52 -0300

    importando componente TransactionsTable

--- src/components/Dashboard/index.tsx
+++ src/components/Dashboard/index.tsx
@@ -1,10 +1,12 @@
 import { Container } from "./styles";
 import { Summary } from "../Summary";
+import { TransactionsTable } from "../TransactionsTable";
 
 export function Dashboard() {
   return (
     <Container>
       <Summary />
+      <TransactionsTable/>
     </Container>
   );
 }
