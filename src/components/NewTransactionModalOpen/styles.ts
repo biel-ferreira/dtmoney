6888aac4e0978555a0605a306f2a164d20ac2f2b
biel-ferreira <93892548+biel-ferreira@users.noreply.github.com>
2021-12-23 00:02:52 -0300

    selecionando a cor do botão

--- src/components/NewTransactionModalOpen/styles.ts
+++ src/components/NewTransactionModalOpen/styles.ts
@@ -1,5 +1,5 @@
 import styled from "styled-components";
-import {darken} from "polished";
+import {darken, transparentize} from "polished";
 
 export const Container = styled.form`
     h2{
@@ -54,13 +54,18 @@ export const TransactionTypeContainer = styled.div`
 `   
 interface RadioBoxProps {
     isActive: boolean;
+    activeColor: 'green' | 'red';
+}
+const colors = {
+    green: '#33cc95',
+    red: '#e5254d'
 }
 
 export const RadioBox = styled.button<RadioBoxProps>`
     height: 4rem;
         border: 1px solid #d7d7d7;
         border-radius: 0.25rem;
-        background: transparent;
+        background: ${(props)=> props.isActive ? transparentize(0.9, colors[props.activeColor]) : "transparent"};
         display: flex;
         align-items: center;
         justify-content: center;
