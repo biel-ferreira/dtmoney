b0e4a7525c6c76611b7c6eb037782e5b570c09f6
biel-ferreira <93892548+biel-ferreira@users.noreply.github.com>
2021-12-23 00:02:23 -0300

    criação do banco de dados integrado do miragejs

--- src/index.tsx
+++ src/index.tsx
@@ -1,23 +1,48 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { createServer } from 'miragejs';
+import { createServer, Model } from 'miragejs';
 import {App} from './App';
 
 createServer({
+  
+  models: {
+    transaction: Model,
+
+  },
+
+  seeds(server){
+    server.db.loadData({
+      transactions: [{
+        id:1,
+        title: 'Freelancer de website',
+        type: 'deposit',
+        category: 'Developer',
+        amount: 6000,
+        createdAt: new Date('2021-02-18 09:00:00')
+      },
+      {
+        id:2,
+        title: 'Aluguel',
+        type: 'withdraw',
+        category: 'Casa',
+        amount: 1100,
+        createdAt: new Date('2020-05-16 12:00:00')
+      }]
+    })
+  },
+
   routes(){
     this.namespace = 'api';
 
     this.get('/transactions', () => {
-      return(
-        {
-          id: 1,
-          title: 'Transaction 1',
-          amount: 400,
-          type: 'deposit',
-          category: 'Food',
-          createdAt: new Date()
-        }
-      )
+      return this.schema.all('transaction')
+      
+      
+    })
+    this.post('/transactions', (schema, request) => {
+      const data = JSON.parse(request.requestBody)
+
+      return schema.create('transaction', data);
     })
   }
 })
