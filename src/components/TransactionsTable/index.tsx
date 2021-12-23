6819c97ae19f27d94e978430a445cb2b4bb73fad
biel-ferreira <93892548+biel-ferreira@users.noreply.github.com>
2021-12-23 13:26:20 -0300

    transferindo funções dos componentes para a context

--- src/components/TransactionsTable/index.tsx
+++ src/components/TransactionsTable/index.tsx
@@ -1,23 +1,11 @@
-import { useEffect, useState } from "react";
-import { api } from "../../services/api";
+
+import { useTransactions } from "../../hooks/useTransactions";
+
 import { Container } from "./styles";
 
-type transactionType = {
-  id: number;
-  title: string;
-  amount: number;
-  type: string;
-  category: string;
-  createdAt: string;
-};
 
 export function TransactionsTable() {
-  const [transactions, setTransactions] = useState<transactionType[]>([]);
-  useEffect(() => {
-    api
-      .get("transactions")
-      .then((response) => setTransactions(response.data.transactions));
-  }, []);
+  const {transactions} = useTransactions();
   return (
     <Container>
       <table>
